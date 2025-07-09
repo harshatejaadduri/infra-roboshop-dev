aef0a4f44dbc641dcc26f0670572a3c6ee9de677
harshatejaadduri <143252561+harshatejaadduri@users.noreply.github.com>
2025-07-09 12:03:53 +0530

    updated

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,10 +3,21 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
-
+  iam_instance_profile = "TerrafromEc2Access"
+  
   
   tags = merge( local.common_tags, {
     Name = "${var.project}-${var.environment}-bastion"
   }
   )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+   aws_instance.bastion.id
+  ]
+   provisioner "local-exec" {
+   command = "sudo dnf install ansible -y | git clone https://github.com/harshatejaadduri/infra-roboshop-dev.git" 
+  }
+  depends_on = [ aws_instance.bastion ]
 }
